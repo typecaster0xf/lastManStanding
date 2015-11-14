f44240328333e2dc71d8557f9d4da0e38d7c3269
Norman Smill <norman.smill@gmail.com>
2015-11-13 22:37:29 -0800

    Commented the main.cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,26 @@
 
 using namespace std;
 
+/**
+Recursively tries all possible moves until a
+winning set of moves is on the stack.  When the
+stack is collapsed, it will print out the winning
+strategy (in reverse) to the console.
+
+Params:
+gameBoard= A GameBoard set to whatever positions
+will be evaluated in this recursive iteration.
+This will either be the return from the default
+constructor or from the makeMove function.
+
+Returns: 'true' if a winning strategy has
+occurred, 'false' if all recursive possibilities
+were tried and no winning strategy was stumbled
+upon.
+*/
 bool playGame(GameBoard gameBoard);
+
+//===============================================
 
 int main()
 {
@@ -19,12 +38,12 @@ bool playGame(GameBoard gameBoard)
 	const unsigned int numberOfMoves =
 			gameBoard.getPossibleNumberOfMoves();
 	
-	if(numberOfMoves == 0)
-		return false;
-	
 	for(unsigned int j = 0; j < numberOfMoves; j++)
 		if(playGame(gameBoard.makeMove(j)))
-		{
+		{/*Print put the game board at this level
+		in the stack to represent this move to
+		the user.*/
+			
 			cout << gameBoard <<
 					"=========================================\n";
 			return true;
